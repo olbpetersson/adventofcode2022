76ad1b081a7ec78e583232296bb2739b278e2397
Ola Petersson <ola.petersson@meltwater.com>
2022-12-05 18:42:06 +0100

    newline parsing aka widar parsing

--- lib/src/main/kotlin/adventofcode2022/Day5.kt
+++ lib/src/main/kotlin/adventofcode2022/Day5.kt
@@ -5,9 +5,10 @@ package adventofcode2022
 
 
 fun main() {
-    val lines = readFile("/day5").readLines()
-    val (moveIndex, stacks) = getMoveIndexAndStacks(lines)
-    lines.subList(moveIndex, lines.size).forEach { strategyLine ->
+    val input = readFileText("/day5")
+    val (stacksInput, strategyInput) = input.split("\n\n")
+    val stacks = getStacks(stacksInput)
+    strategyInput.trim().lines().forEach { strategyLine ->
         val instructions = strategyLine.split(" ")
         val quantity = instructions[1].toInt()
         val from = instructions[3].toInt() - 1
@@ -24,11 +25,9 @@ fun main() {
 
 }
 
-private fun getMoveIndexAndStacks(lines: List<String>): Pair<Int, MutableList<ArrayDeque<String>>> {
+private fun getStacks(stacksInput: String): java.util.ArrayList<ArrayDeque<String>> {
     val dequeList = mutableListOf<ArrayDeque<String>>()
-    var moveIndex = 0
-    while (!lines[moveIndex].startsWith("move")) {
-        val line = lines[moveIndex]
+    stacksInput.lines().forEach { line ->
         if (line.isNotEmpty()) {
             val columnsIncludingWhitespace = line.chunked(4)
             columnsIncludingWhitespace.forEachIndexed { columnIndex, column ->
@@ -38,10 +37,9 @@ private fun getMoveIndexAndStacks(lines: List<String>): Pair<Int, MutableList<Ar
                 dequeList[columnIndex].add(column)
             }
         }
-        moveIndex++
     }
     val cleanedDequeList = dequeList.map { it ->
         ArrayDeque(it.filter { it.isNotBlank() && !it.trim().first().isDigit() })
     } as ArrayList
-    return moveIndex to cleanedDequeList
+    return cleanedDequeList
 }
--- lib/src/main/kotlin/adventofcode2022/util.kt
+++ lib/src/main/kotlin/adventofcode2022/util.kt
@@ -4,3 +4,6 @@ import java.io.File
 
 fun readFile(path: String): File =
     File(object {}.javaClass.getResource(path).file)
+
+fun readFileText(path: String): String =
+    object {}.javaClass.getResource(path).readText()
