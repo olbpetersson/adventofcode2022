347458e3d5872895d56664314a690c2a8e324421
Ola Petersson <ola.petersson@meltwater.com>
2022-12-10 23:52:34 +0100

    day 9 2

--- lib/src/main/kotlin/adventofcode2022/Day9.kt
+++ lib/src/main/kotlin/adventofcode2022/Day9.kt
@@ -1,8 +1,11 @@
 package adventofcode2022
 
-data class Coordinate(var x: Int, var y: Int)
+import java.lang.IllegalStateException
+import kotlin.math.sign
+
+data class Coordinate(var x: Int = 0, var y: Int = 0)
 class Day9(val lines: List<String>) {
-    val tailsVisited = mutableSetOf<Coordinate>()
+    private val tailsVisited = mutableSetOf<Coordinate>()
 
     fun partOne() {
         var headPosition = Coordinate(0, 0)
@@ -18,7 +21,7 @@ class Day9(val lines: List<String>) {
                     "U" -> headPosition.y++
                     "D" -> headPosition.y--
                 }
-                if(tailShouldMove(headPosition, tailPosition)) {
+                if (tailShouldMove(headPosition, tailPosition)) {
                     tailPosition = tempHead
                     tailsVisited.add(tailPosition)
                 }
@@ -27,15 +30,46 @@ class Day9(val lines: List<String>) {
         println(tailsVisited.size)
     }
 
-    private fun tailShouldMove(headPosition: Coordinate, tailPosition: Coordinate): Boolean {
-        val horisontalDifference = Math.abs(headPosition.x - tailPosition.x)
-        val verticalDifference = Math.abs(headPosition.y - tailPosition.y)
-
-        return headPosition != tailPosition && (horisontalDifference > 1 || verticalDifference > 1)
+    fun partTwo() {
+        val connectedHeadTailList = mutableListOf<Coordinate>()
+        connectedHeadTailList.addAll((1..10).map { Coordinate() })
+        lines.forEach { input ->
+            val (direction, value) = input.split(" ")
+            // Move head first
+            // then check the rest as head-tail pair
+            // IF tail should move, move it
+            // add last tailS
+            repeat(value.toInt()) {
+                when (direction) {
+                    "R" -> ++connectedHeadTailList.first().x
+                    "L" -> --connectedHeadTailList.first().x
+                    "U" -> ++connectedHeadTailList.first().y
+                    "D" -> --connectedHeadTailList.first().y
+                    else -> throw IllegalStateException()
+                }
+                connectedHeadTailList.subList(0, connectedHeadTailList.size).windowed(2)
+                    .forEach { (currentHead, currentTail) ->
+                        if (tailShouldMove(currentHead, currentTail)) {
+                            currentTail.x += (currentHead.x - currentTail.x).sign
+                            currentTail.y += (currentHead.y - currentTail.y).sign
+                        }
+                    }
+                tailsVisited.add(connectedHeadTailList.last().copy())
+            }
+        }
+        println(tailsVisited.size)
     }
+
+}
+
+private fun tailShouldMove(headPosition: Coordinate, tailPosition: Coordinate): Boolean {
+    val horisontalDifference = Math.abs(headPosition.x - tailPosition.x)
+    val verticalDifference = Math.abs(headPosition.y - tailPosition.y)
+
+    return (horisontalDifference > 1 || verticalDifference > 1)
 }
 
 
 fun main() {
-    Day9(readFileText("/day9").lines()).partOne()
+    Day9(readFileText("/day9").lines()).partTwo()
 }
