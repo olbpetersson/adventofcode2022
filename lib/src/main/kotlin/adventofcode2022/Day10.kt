372f1aa9875710e80d94936e621992e0910ae2ca
Ola Petersson <ola.petersson@meltwater.com>
2022-12-11 14:56:46 +0100

    day10 2

--- lib/src/main/kotlin/adventofcode2022/Day10.kt
+++ lib/src/main/kotlin/adventofcode2022/Day10.kt
@@ -7,6 +7,41 @@ data class Add(override val value: Int) : Instructions(2)
 data class Noop(override val value: Int = 0) : Instructions(1)
 
 fun main() {
+    partTwo()
+}
+
+fun partTwo() {
+    val lines = readFileText("/day10").lines()
+    val instructions: List<Instructions> = lines.map {
+        val text = it.split(" ")
+        when {
+            text[0].startsWith("addx") -> Add(text[1].toInt())
+            text[0].startsWith("noop") -> Noop()
+            else -> throw IllegalArgumentException()
+        }
+    }
+    var registerValue = 1
+    var numberOfCycles = 0
+    var cycleString = ""
+    instructions.forEach { instruction ->
+        repeat(instruction.cycles) {
+            numberOfCycles++
+            val normalizedCycle = numberOfCycles%40
+            if(normalizedCycle-1 in (registerValue-1..registerValue+1)) {
+                cycleString+="#"
+            } else {
+                cycleString+="."
+            }
+            if(normalizedCycle == 0) {
+                println("$cycleString ${cycleString.length} at $numberOfCycles")
+                cycleString = ""
+            }
+
+        }
+        registerValue += instruction.value
+    }
+}
+fun partOne() {
     val lines = readFileText("/day10").lines()
     val instructions: List<Instructions> = lines.map {
         val text = it.split(" ")
